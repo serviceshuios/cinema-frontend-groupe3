966d75d2b16bea6a923fed73aae9967148c3d863
Quentin RYCKEBUSCH <quentin.ryckebusch@protonmail.com>
2020-01-14 10:12:34 +0100

    ajout chemin ville et categorie

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FilmComponent } from './component/film/film.component';
+import { VilleComponent } from './component/ville/ville.component';
+import { CategorieComponent } from './component/categorie/categorie.component';
 
 // définition des routes du projet
 const routes: Routes = [
   { path: 'film', component: FilmComponent },
+  { path: 'ville', component: VilleComponent },
+  { path: 'categorie', component: CategorieComponent }
 
 
   // déclaration de la route par défaut
