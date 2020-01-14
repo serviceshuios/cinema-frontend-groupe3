cceca87cf7359ec889135e9795ac81d6e25ffe90
Giunio DE LUCA <giuniodl@live.it>
2020-01-14 09:59:41 +0100

    ajout modules dans app.module.ts

---
+++ src/app/models/film.model.ts
@@ -0,0 +1,9 @@
+export class Film {
+	public idFilm: number;
+	public titre: String;
+	public duree: number;
+	public realisateur: String;
+	public description: String;
+	public photo: String;
+	public dateSortie: Date;
+}
