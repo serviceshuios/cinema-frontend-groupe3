d42746911db419d8bb68e404f4702796989ed9c2
Corentin BOUET <corentinbouet@orange.fr>
2020-01-14 13:43:41 +0100

    Ajout classe Seance

---
+++ src/app/service/seance.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Seance } from '../models/seance.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SeanceService {
+
+  public host = 'http://localhost:8082';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getAllSeances(): Observable<Seance> {
+    return this.httpClient.get<Seance>(this.host + '/seances/');
+  } // fin getAll
+
+  public getSeance(id: number): Observable<Seance> {
+    return this.httpClient.get<Seance>(this.host + '/seances/' + id);
+  } // fin getById
+
+  public addSeance(seance: Seance) {
+    return this.httpClient.post<Seance>(this.host + '/seances/', seance);
+  } // fin add
+
+  public deleteSeance(id: number) {
+    return this.httpClient.delete<Seance>(this.host + '/seances/' + id);
+  }// fin delete
+
+}
